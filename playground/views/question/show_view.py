780060fc3f2b9814c0c7e8cc8c73f4f1ac2f3960
thori0908 <mr.takumi.pc.0908@gmail.com>
2024-12-05 19:11:12 -0800

    fix OpenAPI schema

--- playground/views/question/show_view.py
+++ playground/views/question/show_view.py
@@ -3,8 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from playground.models import Question
 from training.serializers import QuestionSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 class ShowView(APIView):
+    @swagger_auto_schema(
+        responses={200: QuestionSerializer()}
+    )
     def get(self, request, *args, **kwargs) -> Response:
         id = kwargs.get('id')
         question = Question(id=id, question_text='What is the meaning of life?', pub_date='2022-01-01')
--- training/serializers.py
+++ training/serializers.py
@@ -5,3 +5,10 @@ class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'question_text', 'pub_date']
+
+
+# NOTE: when using serializers.Serializer
+# class QuestionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     question_text = serializers.CharField()
+#     pub_date = serializers.DateField()
